81c10b86f8b121cf238cf32f65a7a0b4da03c473
Sohail Aziz <sohail.aziz05@gmail.com>
2018-12-31 19:01:46 +0500

    adds support to change toolbar and button color using Paris

---
+++ app/src/main/java/com/example/sohailaziz/dynamictheming/Main2Activity.java
@@ -0,0 +1,73 @@
+package com.example.sohailaziz.dynamictheming;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+import com.airbnb.paris.Paris;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        int themeColorPrimary = getColor();
+
+        getWindow().setStatusBarColor(getColor());
+
+        Paris.styleBuilder(toolbar)
+                .background(new ColorDrawable(themeColorPrimary))
+                .apply();
+
+        Button button = findViewById(R.id.button);
+        Paris.styleBuilder(button)
+                .background(new ColorDrawable(themeColorPrimary))
+                .apply();
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private int getColor() {
+        Random rnd = new Random();
+        return Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+}
---
+++ app/src/main/java/com/example/sohailaziz/dynamictheming/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sohailaziz.dynamictheming;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
+    }
+
+
+}
